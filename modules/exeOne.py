76705dbeb74c1936655930f2f64b282318db6c96
Jose Barranco <jdcoding01@gmail.com>
2018-09-17 17:32:56 -0400

    Update exeOne.py

--- modules/exeOne.py
+++ modules/exeOne.py
@@ -1,32 +1,5 @@
 #! /usr/bin/python3
-# _Programa : wolf-wep_
-# _Version :1.0_
-# _Creador : Gabriel Romero_
-# _Contacto : gabrielromero0499@gmail.com_
-# _Apodo : Wolf-Saiko_
 
-#################################################################
-#                                                               #
-# -*- ENCODING: UTF-8 -*-                                       #
-# Este script es software libre. Puede redistribuirlo y/o       #
-# modificar-lo bajo los términos de la Licencia Pública General #
-# de GNU según es publicada por la Free Software Foundation,    #
-# bien de la versión 3 de dicha Licencia o bien (según su       #
-# elección) de cualquier versión posterior.                     #
-#                                                               #
-# Si usted hace alguna modificación en esta aplicación,         #
-# deberá siempre mencionar al autor original de la misma.       #
-#                                                               #
-# Autor: Script creado por Wolf-Saiko                           #
-#                                                               #
-# Integra Funciones De:                                         #
-#                                                               #
-# Python 3                                                      #
-# Aircrack-ng                                                   #
-# Xterm                                                         #
-#                                                               #
-# Un saludo                                                     # 
-#                                                               #
 #################################################################""")
 
 import os
